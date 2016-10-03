83327f8af60ca6dfcfa0d42d2b2d046eae321aca
Alon Niv <oakfang@gmail.com>
2016-10-03 16:45:48 +0100

    bump to 2.0

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
-const { intent, interpret, ensure } = require('intention')();
+const { intent, interpret, isIntent } = require('intention')();
 const DEFAULT_VALUE_SYM = Symbol('@@default');
 const MISSING_VALUE_SYM = Symbol('@@missing');
 
-const monads = new WeakMap();
-
 const monad = (type, ctors) => Object.keys(ctors).reduce((result, instance) => {
-    result[instance] = (...args) => {
-      const monadicValues = {
-        args,
-        type,
-        instance,
-        ctor: ctors[instance],
-      };
-      const monadicValue = intent(`monad:value`, monadicValues);
-      monads.set(monadicValue, monadicValues);
-      return monadicValue;
-    };
+    result[instance] = (...args) => intent(`monad:value`, {
+      args,
+      type,
+      instance,
+      ctor: ctors[instance],
+    });
 
     return result;
   }, Object.create(null));
 
 const match = (monadicValue, matches, defaultValue=DEFAULT_VALUE_SYM) => {
-  if (!monads.has(monadicValue)) throw new Error('Cannot match a non-monad');
+  if (!isIntent(monadicValue)) throw new Error('Cannot match a non-monad');
   if (defaultValue === DEFAULT_VALUE_SYM) return intent('monad:match', {
     monadicValue,
     matches,
   });
-  const { args, instance } = monads.get(monadicValue);
+  const { args, instance } = monadicValue.values;
   if (instance in matches) return matches[instance](...args);
   return defaultValue;
 };
@@ -50,7 +43,7 @@ const monadicReality = Object.freeze({
 });
 
 const enableMonads = interpretF => (intentObject, reality) =>
-  (ensure(intentObject) ?
+  (isIntent(intentObject) ?
     interpret(intentObject, monadicReality) :
     interpretF(intentObject, reality));
 
